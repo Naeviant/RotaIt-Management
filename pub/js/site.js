aeb0ce2f57fbbee85d13d4ea75d36ae5141c46aa
Naeviant <Naeviant@outlook.com>
2019-05-03 17:49:01 +0100

    Added Staff Management HTML

--- pub/js/site.js
+++ pub/js/site.js
@@ -2,3 +2,11 @@
 $(document).ready(function() {
     M.AutoInit();
 });
+
+// Prevent Vertically Aligned Elements from Overflowing
+$(document).ready(function() {
+    console.log($(".valign-wrapper>.col").height(), $(window).height() - 100)
+    if ($(".valign-wrapper>.col").height() > $(window).height() - 100) {
+        $(".valign-wrapper").removeClass("valign-wrapper");
+    }
+});
